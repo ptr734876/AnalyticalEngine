369e7d3e6f2c0fb4eefd79f4111d291fb56c545b
ptr734876 <oblomkov05@gmail.com>
2025-10-09 00:49:38 +0300

    0.0.005

--- visualization/d2.py
+++ visualization/d2.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
-def line2d(x, y, xname='x', yname='y', label=''):
+def line2d(x, y, xname='x', yname='y', label='', title=''):
     plt.plot(x, y, label)
 
     plt.xlabel(xname)
     plt.ylabel(yname)
+    plt.title(title)
     plt.legend()
     plt.show()
