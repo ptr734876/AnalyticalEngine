6801c060fa072138cd75bc806795cff1b878059a
ptr734876 <oblomkov05@gmail.com>
2025-10-19 15:53:15 +0300

    0.0.013

--- algorithms/neyro.py
+++ algorithms/neyro.py
@@ -2,9 +2,14 @@ import numpy as np
 
 class BaseNeyro:
     def __init__(self, inx, outy, layers_number, task='classification'):
-        np.random.seed(42)  # Set seed for reproducible results
-        self.inx = (inx - np.mean(inx, axis=0)) / np.std(inx, axis=0)
-        self.TrueY = (outy - np.mean(outy, axis=0)) / np.std(outy, axis=0)
+        np.random.seed(42)
+        self.inx_mean = np.mean(inx, axis=0)
+        self.inx_std = np.std(inx, axis=0) + 1e-8
+        self.outy_mean = np.mean(outy, axis=0)
+        self.outy_std = np.std(outy, axis=0) + 1e-8
+
+        self.inx = (inx - self.inx_mean) / self.inx_std
+        self.TrueY = (outy - self.outy_mean) / self.outy_std
         self.task = task
         if isinstance(layers_number, list):
             self.neurns = layers_number
@@ -13,19 +18,24 @@ class BaseNeyro:
 
         lower_digits = 0.1
         self.ws = []
-        self.ws.append(np.random.randn(self.inx.shape[1], self.neurns[0]) * np.sqrt(1 / self.inx.shape[1]))
-        
+        self.bs = []
+        self.ws.append(np.random.randn(self.inx.shape[1], self.neurns[0]) * lower_digits)
+        self.bs.append(np.zeros((1, self.neurns[0])))
+
         if len(self.neurns) == 0:
-            self.ws.append(np.random.randn(self.inx.shape[1], self.TrueY.shape[1]) * np.sqrt(1 / self.inx.shape[1]))
+            self.ws.append(np.random.randn(self.inx.shape[1], self.TrueY.shape[1]) * lower_digits)
 
         elif len(self.neurns) == 1:
-            self.ws.append(np.random.randn(self.neurns[0], self.TrueY.shape[1]) * np.sqrt(1 / self.neurns[0]))
+            self.ws.append(np.random.randn(self.neurns[0], self.TrueY.shape[1]) * lower_digits)
 
         else:        
             for i in range(1, len(self.neurns)):
-               self.ws.append(np.random.randn(self.neurns[i-1], self.neurns[i]) * np.sqrt(1 / self.neurns[i-1]))
-            self.ws.append(np.random.randn(self.neurns[-1], self.TrueY.shape[1]) * np.sqrt(1 / self.neurns[-1]))
-    
+               self.ws.append(np.random.randn(self.neurns[i-1], self.neurns[i]) * lower_digits)
+               self.bs.append(np.zeros((1, self.neurns[i])))
+
+            self.ws.append(np.random.randn(self.neurns[-1], self.TrueY.shape[1]) * lower_digits)
+            self.bs.append(np.zeros((1, self.TrueY.shape[1])))
+
     @staticmethod
     def softmax(x):
         e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
@@ -78,25 +88,34 @@ class BaseNeyro:
     act_methods = [softmax, sigmoid, tanh, relu, linear]
     act_der_methods = [softmax_der, sigmoid_der, tanh_der, relu_der, lin_der]
 
-    def forward(self, act_layers=sigmoid, end_layer=softmax):
+    def forward(self, act_layers=sigmoid, end_layer=softmax, input_data=None):
         
-        self.activation = [self.inx]
+        if input_data is not None:
+            norm_data = (input_data - self.inx_mean) / self.inx_std
+            self.activation = [norm_data]
+        else:
+            self.activation = [self.inx]
         self.zs = []
 
         cur = self.inx
 
-        for i, w in enumerate(self.ws):
-            z = cur @ w
+        for i in range(len(self.ws) - 1):
+            z = cur @ self.ws[i] + self.bs[i]
             self.zs.append(z)
-
-            if i == len(self.ws) - 1:
-                cur = end_layer(z)
-            else:
-                cur = act_layers(z)
-
+            cur = act_layers(z)
             self.activation.append(cur)
 
+        z = cur @ self.ws[-1] + self.bs[-1]
+        self.zs.append(z)
+        cur = end_layer(z)
+        self.activation.append(cur)
+
         return cur
+    
+    def predict(self, X, act_layers=relu, end_layer=linear):
+        norm_out = self.forward(act_layers=act_layers, end_layer=end_layer, input_data=X)
+        denorm_out = norm_out * self.outy_std + self.outy_mean
+        return denorm_out
     
     def mse(y_true, y_pred):
         return np.mean((y_true - y_pred)**2)*(1/2)
@@ -104,31 +123,27 @@ class BaseNeyro:
     def mse_der(y_true, y_pred, batch_size=1):
         return (y_pred - y_true) / batch_size
     
-    def backward(self, act_layers=None, end_layer=None, lmd=0.01, error_func=mse, batch_size=1):
-
+    def backward(self, act_layers=None, end_layer=None, lmd=0.01, error_func=None, batch_size=1):
         if act_layers is None:
-            if self.task == 'classification':
-                act_layers = BaseNeyro.sigmoid
-
-                act_der = BaseNeyro.sigmoid_der
-            elif self.task == 'regression':
-                act_layers = BaseNeyro.relu
-                act_der = BaseNeyro.relu_der
-            else:
-                raise ValueError(f'{self.task} is unknown task type')
-        elif act_layers is not None:
-            if act_layers in BaseNeyro.act_methods:
-                act_der = BaseNeyro.act_der_methods[BaseNeyro.act_methods.index(act_layers)]
-            else:
-                raise ValueError(f'{act_layers} is unknown name')
-
+            act_layers = BaseNeyro.relu if self.task == 'regression' else BaseNeyro.sigmoid
         if end_layer is None:
-            if self.task == 'classification':
-                end_layer = BaseNeyro.softmax
-                end_der = BaseNeyro.softmax_der
-            elif self.task == 'regression':
-                end_layer = BaseNeyro.linear
-                end_der = BaseNeyro.lin_der
+            end_layer = BaseNeyro.linear if self.task == 'regression' else BaseNeyro.softmax
+        if error_func is None:
+            error_func = BaseNeyro.mse
+        
+        if act_layers == BaseNeyro.sigmoid:
+            act_der = BaseNeyro.sigmoid_der
+        elif act_layers == BaseNeyro.relu:
+            act_der = BaseNeyro.relu_der
+        elif act_layers == BaseNeyro.tanh:
+            act_der = BaseNeyro.tanh_der
+        else:
+            act_der = BaseNeyro.lin_der
+        
+        if end_layer == BaseNeyro.softmax:
+            end_der = lambda x: 1
+        else:
+            end_der = BaseNeyro.lin_der
 
         out = self.forward(act_layers=act_layers, end_layer=end_layer)
         error = self.TrueY - out
@@ -136,7 +151,7 @@ class BaseNeyro:
         if self.task == 'classification': # 1 | разделение по типу задачи - классификация
 
             if end_layer == BaseNeyro.softmax: # проверка на наличие софт макса на слое выхода (только его произваодня прописана в коде)
-                delta = out - self.TrueY #cross entropy loss + softamx | out grad | TrueY - one hot encoded метка
+                delta = (out - self.TrueY) / self.TrueY.shape[0] #cross entropy loss + softamx | out grad | TrueY - one hot encoded метка
             else:
                 raise ValueError(f'Not support classification with {act_layers, end_layer}') #ошибка при другом методе активации
             
@@ -151,10 +166,13 @@ class BaseNeyro:
             raise ValueError(f'{self.task} is unknown name')
 
         for i in range(len(self.ws)-1, -1, -1):# обратный проход по слоям, от индекса len(self.ws)-1 до -1(тоесть нулевого)
-            grad_w = self.activation[i].T @ delta # проход по весам по прямой это imput  @ ws а для обратного направления это транспарированная матрца активаций на дельту тоесть рахницу 
+            grad_w = self.activation[i].T @ delta # проход по весам по прямой это imput  @ ws а для обратного направления это транспарированная матрца активаций на дельту тоесть рахницу
+            grad_b = np.sum(delta, axis=0, keepdims=True)
+
             self.ws[i] -= lmd * grad_w # мы узнали градиент изменения и умножаем его лямбду чтобы уменьшить шаг изменения, а затем вычитаем это из весов чтобы изменить их
+            self.bs[i] -= lmd * grad_b
 
             if i > 0:# для последнего слоя нет предыдущего слоя чтобы найти ошибку
-                delta = delta @ self.ws[i].T * act_der(self.zs[i])# градиент по входу текущего слоя (delta @ self.ws[i].T) умножается на производную функ активации на текущем слое
+                delta = delta @ self.ws[i].T * act_der(self.zs[i-1])# градиент по входу текущего слоя (delta @ self.ws[i].T) умножается на производную функ активации на текущем слое
         
-        return out, error
+        return out, BaseNeyro.mse(self.TrueY, out)
--- tests/text_neyro.py
+++ tests/text_neyro.py
@@ -3,14 +3,33 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from algorithms.neyro import BaseNeyro
 import numpy as np
 
+
 def test2():
-    f = np.array([[1], [2], [3], [4], [5], [6], [7], [8], [9]])
-    f1 = np.array([[6], [7], [8], [9], [10], [11], [12], [13], [14]])
+    np.random.seed(1)
+    def create_data(n, a, b, func):
+
+        f = np.random.uniform(a, b, size=(n, 1))
+        f1 = func(f)
+        return f, f1
+    def func(f):
+        return f * 2
+    X_train, y_train = create_data(100, -100, 100, func)
+
+    a = BaseNeyro(X_train, y_train, [5, 1], task='regression')
     
-    a = BaseNeyro(f, f1, [4], task='regression')
-    for i in range(30001):
-        c, d = a.backward(lmd=0.01)
+    print("Training...")
+    for i in range(10010):
+        c, error = a.backward(lmd=0.001)
         if i % 1000 == 0:
-            print(np.mean(c), np.mean(d))
+            print(f"Iteration {i}, Error: {error:.6f}")
+    
+    test_input = np.array([[1.5], [3.5], [5.5]])
+    
+    predictions = a.predict(test_input, act_layers=BaseNeyro.relu, end_layer=BaseNeyro.linear)  
+
+    print("\nPredictions:")
+    for i in range(len(test_input)):
+        print(f"Input: {test_input[i][0]}, Expected: {test_input[i][0]*2:.1f}, Predicted: {predictions[i][0]:.2f}")
+
 if __name__ == '__main__':
     test2()
